a1031738a4f3c4969d532bab6cfecb131676ab2e
Chazni KATZ <ckatz@e4r19p5.wtc.co.za>
2017-08-11 18:49:05 +0200

    dimensions of map determined

--- points.c
+++ points.c
@@ -6,55 +6,73 @@
 /*   By: ckatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/11 17:39:25 by ckatz             #+#    #+#             */
-/*   Updated: 2017/08/11 17:41:16 by ckatz            ###   ########.fr       */
+/*   Updated: 2017/08/11 18:46:51 by ckatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <mlx.h>
 #include "libft/libft.h"
 
-int void	fd_error_check(int fd)
+int			fd_error_check(int fd)
 {
 	if (fd < 0)
-	{
 		ft_putstr("Error opening file");
-		return (1);
-	}
+	return (1);
 }
-
-static int	read_cols(int fd)
+/*static int	count_cols(char *line, int i)
 {
-	char	*lines;
-	int		rows;
+	while ()
+}*/
 
+static int	count_rows(int fd)
+{
+	char	*line;
+	int		rows;
+	int		i;
+	int		cols;
+
+	i = 0;
 	rows = 0;
-	while (get_next_line(fd, &lines) == 1)
+	while (get_next_line(fd, &line) == 1)
 	{
 		rows++;
 	}
+	cols = 0;
+	while (line[i])
+	{
+		if (ft_isdigit(line[i]))
+			cols++;
+		i++;
+	}
+	printf("%s\n", line);
+	printf("%d\n", cols);
 	return (rows);
 }
 
 void		read_map()
 {
-	char	*lines;
-	char	**holder;
 	int		fd;
 	int		i;
 	int		num_rows;
+	int		num_cols;
 	
 	fd = open("42.txt", O_RDONLY);
 	fd_error_check(fd);
 	num_rows = 0;
+	num_cols = 0;
 	i = 0;
 	if (fd > 0)
 	{
-
+		num_rows = count_rows(fd);
+		printf("Rows 	:%d\n", num_rows);
+	//	printf("Columns :%d\n", num_rows);
 	}
 	close(fd);
 }
 
 int			main()
 {
+	read_map();
 	return (0);
 }
