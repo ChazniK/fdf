8667e9cad7db14aeca77c17442fdb1737b03848d
ChazniK <chazni.k9@gmail.com>
2017-08-12 11:51:42 +0200

    added functions to obtain map dimensions and malloc accordingly

--- points.c
+++ points.c
@@ -12,7 +12,7 @@
 
 #include <stdio.h>
 #include <mlx.h>
-#include "libft/libft.h"
+#include "fdf.h"
 
 int			fd_error_check(int fd)
 {
@@ -20,59 +20,73 @@ int			fd_error_check(int fd)
 		ft_putstr("Error opening file");
 	return (1);
 }
-/*static int	count_cols(char *line, int i)
-{
-	while ()
-}*/
 
-static int	count_rows(int fd)
+static void	get_map_dim(int fd, t_map *tmap)
 {
 	char	*line;
-	int		rows;
 	int		i;
-	int		cols;
 
 	i = 0;
-	rows = 0;
+	tmap->num_rows = 0;
 	while (get_next_line(fd, &line) == 1)
 	{
-		rows++;
+		tmap->num_rows++;
 	}
-	cols = 0;
+	tmap->num_cols = 0;
 	while (line[i])
 	{
 		if (ft_isdigit(line[i]))
-			cols++;
+			tmap->num_cols++;
 		i++;
 	}
-	printf("%s\n", line);
-	printf("%d\n", cols);
-	return (rows);
+	free(line);
 }
 
-void		read_map()
+void		store_map()
 {
 	int		fd;
-	int		i;
-	int		num_rows;
-	int		num_cols;
+	int		**map_arr;
+	char	*line;
+	t_map	tmap;
+	//int		i;
 	
-	fd = open("42.txt", O_RDONLY);
+	fd = open("42.fdf", O_RDONLY);
 	fd_error_check(fd);
-	num_rows = 0;
-	num_cols = 0;
-	i = 0;
 	if (fd > 0)
 	{
-		num_rows = count_rows(fd);
-		printf("Rows 	:%d\n", num_rows);
-	//	printf("Columns :%d\n", num_rows);
+		get_map_dim(fd, &tmap);
+		printf("point 1\n");
+		map_arr = ft_arr_ints(tmap.num_rows, tmap.num_cols);
+		printf("point 2\n");
+		if (get_next_line(fd, &line) == 1)	
+		{
+			//i = 0;
+			printf("point 3\n");
+			//tmap.data = ft_strsplit(line, ' ');
+			//printf("%s\n", tmap.data);
+			//printf("%s\n", tmap.data[0]);
+		}
+ 		/*int	a = 0;
+		int	b;
+		while (a < tmap.num_rows)
+		{
+			b = 0;
+			while (b < tmap.num_cols)
+			{
+				printf("%d", map_arr[a][b]);
+				b++;
+			}
+			printf("\n");
+			a++;
+		}
+		printf("Rows 	:%d\n", tmap.num_rows);
+		printf("Columns :%d\n", tmap.num_cols);*/
 	}
 	close(fd);
 }
 
 int			main()
 {
-	read_map();
+	store_map();
 	return (0);
 }
