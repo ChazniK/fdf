9a581bf307f15779d62a004eb422cb06901ac8a0
Chazni KATZ <ckatz@e4r18p3.wtc.co.za>
2017-08-13 13:48:41 +0200

    updated header file

--- fdf.h
+++ fdf.h
@@ -6,16 +6,16 @@
 /*   By: ckatz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/11 16:32:47 by ckatz             #+#    #+#             */
-/*   Updated: 2017/08/12 18:24:41 by ckatz            ###   ########.fr       */
+/*   Updated: 2017/08/13 13:39:07 by ckatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-#include "libft/libft.h"
-#include <mlx.h>
-#include <math.h> 
+# include "libft/libft.h"
+# include <mlx.h>
+# include <math.h>
 
 typedef struct	s_line
 {
@@ -24,7 +24,7 @@ typedef struct	s_line
 	float		x2;
 	float		y2;
 	float		dx;
-	float		dy;	
+	float		dy;
 	float		step;
 }				t_line;
 
@@ -32,10 +32,9 @@ typedef struct	s_map
 {
 	int			num_rows;
 	int			num_cols;
-//	int			**map;
-//	int			i;
-//	int			j;
-	char		**data;
+	int			i;
+	int			j;
+	int			**map_arr;
 }				t_map;
 
 typedef struct	s_env
@@ -45,4 +44,3 @@ typedef struct	s_env
 }				t_env;
 
 #endif
-
